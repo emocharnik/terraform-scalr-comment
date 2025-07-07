4aafd5153efb51965cc0079dd8219cfe6ec9f62c
Edgar Mocharnyk <e.mocharnyk@scalr.com>
2025-07-07 17:13:07 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "scalr_environment" "test" {
 
 resource "scalr_workspace" "test" {
   environment_id = scalr_environment.test.id
-  name = "test-workspace-2"
+  name = "test-workspace-3"
 }
 
 output "environment_id" {
